d30f8d543be45e362b927cf2e2549ee41b46195e
Akshita Chadha <akshitachadha01@gmail.com>
2025-06-08 17:16:05 +0530

    Use environment variable for SECRET_KEY

--- quicktask/settings.py
+++ quicktask/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_oafk-77ew=n@$@j3qbg%au_p+9hbv@b&y2c295z62)o(oz=!'
+SECRET_KEY = os.environ.get('SECRET_KEY','dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
